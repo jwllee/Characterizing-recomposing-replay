68a6e88a909f9f462584fbc618522c4c3c12eace
Wai Lam Jonathan Lee <jonathan.wailam.lee@gmail.com>
2018-08-10 15:31:16 -0400

    Updated experiment config files

--- configs/2018-05-01_small/Replay/Batch runner.sh
+++ configs/2018-05-01_small/Replay/Batch runner.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-# sh "./configs/2018-05-01_small/Replay/ROneMostFreqSet-LExclude-S-M15/Runner.sh"
-sh "./configs/2018-05-01_small/Replay/RScore-LAll-S-M15/Runner.sh"
-sh "./configs/2018-05-01_small/Replay/RScore-LGroup-S-M15/Runner.sh"
+sh "./configs/2018-05-01_small/Replay/Monolithic-15Mins/Runner.sh"
+
+# All category
+sh "./configs/2018-05-01_small/Replay/RAll-LAll-S-M15/Runner.sh"
+sh "./configs/2018-05-01_small/Replay/RMostFreq-LAll-S-M15/Runner.sh"
+sh "./configs/2018-05-01_small/Replay/RGraph-LAll-S-M15/Runner.sh"
+sh "./configs/2018-05-01_small/Replay/RRandom-LAll-S-M15/Runner.sh"
+
+# only group category
+sh "./configs/2018-05-01_small/Replay/ROneMostFreqSet-LExclude-S-M15/Runner.sh"
+sh "./configs/2018-05-01_small/Replay/RThreeMostFreqSet-LExclude-S-M15/Runner.sh"
+sh "./configs/2018-05-01_small/Replay/RTenMostFreqSet-LExclude-S-M15/Runner.sh"
 sh "./configs/2018-05-01_small/Replay/RScore-LExclude-S-M15/Runner.sh"
-# sh "./configs/2018-05-01_small/Replay/RGraph-LGroup-S-M15/Runner.sh"
-# sh "./configs/2018-05-01_small/Replay/RTenMostFreqSet-LExclude-S-M15/Runner.sh"
-# sh "./configs/2018-05-01_small/Replay/RThreeMostFreqSet-LExclude-S-M15/Runner.sh"
-# sh "./configs/2018-05-01_small/Replay/RFiveMostFreqSet-LExclude-S-M15/Runner.sh"
-# sh "./configs/2018-05-01_small/Replay/RFiveConflictSetGraph-LExclude-S-M15/Runner.sh"
